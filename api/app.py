a5d813d590365f0d99db7a80ef208154e47aca69
BenjaminOyarzun17 <benjaoyama@gmail.com>
2021-06-03 22:12:14 -0400

    database

--- api/app.py
+++ api/app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id= db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    direccion = db.Column(db.String(100))
+    def __init__(self, name, email, direccion):
+        self.name = name
+        self.email = email
+        self.direccion = direccion
+
+
  
 @app.route('/pymes',methods= ['GET'])
 def datosPymes():
@@ -26,18 +44,23 @@ def datosPymes():
 }
 
 
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
 
-@app.route('/register',methods= ['POST'])
-def registro():
-    return 0
-
-
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      
+      return redirect(url_for('success',name = request))
+  
 
 
 
 
 
 if __name__== '__main__':
+    db.create_all()
     app.run(debug=True)
 
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Form, Button,} from 'react-bootstrap';
 
 
 class Login extends React.Component{
@@ -8,27 +7,13 @@ class Login extends React.Component{
     render(){
         return (
             <>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Correo</Form.Label>
-                    <Form.Control type="email" placeholder="Ingresar email" />
-                    <Form.Text className="text-muted">
-                        Tú correo no será compartido con nadie más
-                    </Form.Text>
-                    </Form.Group>
-                
-                    <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Contraseña" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Recordar mi contraseña" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                    Iniciar sesión
-                    </Button>
-                </Form>
-            
+            <div>
+            <form action="#" method='POST'>
+                <input type = 'email' placeholder='ingresar mail' value=''/>
+                <input type = 'password' placeholder='ingresar clave' value=''/>
+                <input type='submit' value="submit"/>
+            </form> 
+            </div>
             </>
         )
 
